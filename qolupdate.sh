2e286f019b1e5d070e2f2b3a31cf9a07c27986d3
bobbyphillippsms <149193513+bobbyphillippsms@users.noreply.github.com>
2023-11-17 12:00:28 -0700

    Update qolupdate.sh

--- qolupdate.sh
+++ qolupdate.sh
@@ -1,8 +1,8 @@
-chmod +x ~/Desktop/dellQOL/qolupset.sh
-
 chmod +x ~/Desktop/dellQOL/gitup.sh
 
 source ~/Desktop/dellQOL/gitup.sh
+
+chmod +x ~/Desktop/dellQOL/qolupset.sh
 
 source ~/Desktop/dellQOL/qolupset.sh
 
