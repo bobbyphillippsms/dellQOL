77601a80687688b8bef539083629dedb5b588498
bobbyphillippsms <149193513+bobbyphillippsms@users.noreply.github.com>
2023-11-17 12:21:38 -0700

    Update jtaliases.sh

--- jtaliases.sh
+++ jtaliases.sh
@@ -88,7 +88,7 @@ alias ndh='dhcpdclear'
 ###
 ######
 
-alias qol='~/Desktop/dellQOL/qolupdate.sh && exit 0'
+alias qol='~/Desktop/dellQOL/qolupdate.sh && cd'
 
 alias resfix='~/Desktop/dellQOL/resfix.sh'
 
