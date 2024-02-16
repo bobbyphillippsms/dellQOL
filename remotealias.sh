f4399dd310ff1ff5d729d4f5f2959ff4c0ecd37f
bobbyphillippsms <149193513+bobbyphillippsms@users.noreply.github.com>
2024-02-16 11:30:21 -0700

    Update remotealias.sh

--- remotealias.sh
+++ remotealias.sh
@@ -1,13 +1,34 @@
-alias cp='cp -i'
-alias dcs='cd /tmp/PXE_Scripts/; /tmp/PXE_Scripts/Launch_sustaining.sh'
-alias l.='ls -d .* --color=auto'
-alias ll='ls -l --color=auto'
-alias ls='ls --color'
-alias mv='mv -i'
+# .bashrc
+
+#ignore local popups
+[[ $- != *i* ]] && return
+
+# User specific aliases and functions
+
 alias rm='rm -i'
-alias scp='scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
+alias cp='cp -i'
+alias mv='mv -i'
 alias ssh='ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
-alias which='alias | /usr/bin/which --tty-only --read-alias --show-dot --show-tilde'
+alias scp='scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
+alias dcs='cd /tmp/PXE_Scripts/; /tmp/PXE_Scripts/Launch_sustaining.sh'
+
+export PS1='\[\e[0;36m\][\u@\h \W]#\[\e[m\]'
+
+export VM=192.168.1.1
+export HOMEBASE=$VM
+export DBHOST=$VM
+export PXEVM=$VM
+
+# Source global definitions
+if [ -f /etc/bashrc ]; then
+        . /etc/bashrc
+fi
+
+#Local menu popups
+cd /tmp/PXE_Scripts/
+/tmp/PXE_Scripts/healthcheck.sh
+/tmp/PXE_Scripts/Launch_sustaining.sh
+
 
 ratascript() {
 
