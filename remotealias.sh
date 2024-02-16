234756fe2fbdaa9d9da2c145e7b0b33f24a2325b
bobbyphillippsms <149193513+bobbyphillippsms@users.noreply.github.com>
2024-02-16 11:16:37 -0700

    Update remotealias.sh

--- remotealias.sh
+++ remotealias.sh
@@ -1,3 +1,14 @@
+alias cp='cp -i'
+alias dcs='cd /tmp/PXE_Scripts/; /tmp/PXE_Scripts/Launch_sustaining.sh'
+alias l.='ls -d .* --color=auto'
+alias ll='ls -l --color=auto'
+alias ls='ls --color'
+alias mv='mv -i'
+alias rm='rm -i'
+alias scp='scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
+alias ssh='ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
+alias which='alias | /usr/bin/which --tty-only --read-alias --show-dot --show-tilde'
+
 ratascript() {
 
 	for i in {a..j};
